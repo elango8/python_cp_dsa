381fd60b346550db8cd45c0b888c046cc745e821
Elango Selvan <elangoselvan46@gmail.com>
2026-02-23 18:56:35 +0530

    python problems

---
+++ Find all dup.py
@@ -0,0 +1,18 @@
+def findDuplicates(nums):
+    duplicates = []
+    
+    for num in nums:
+        index = abs(num) - 1 
+        
+        if nums[index] < 0:
+        
+            duplicates.append(abs(num))
+        else:
+        
+            nums[index] = -nums[index]
+    
+    return duplicates
+
+
+nums = [4, 3, 2, 7, 8, 2, 3, 1]
+print(findDuplicates(nums)) 
